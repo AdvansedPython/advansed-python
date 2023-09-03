7e98cfa0fd8400f0e4d1896e54daa6dc3829e5ac
neotyanka <141932038+neotyanka@users.noreply.github.com>
2023-09-03 21:41:27 +0300

    Update 2_Task.py

--- 2_seminar/DZ/2_Task.py
+++ 2_seminar/DZ/2_Task.py
@@ -1,9 +1,29 @@
 import fractions
 
+def Reduction(numerator, denominator):
+    while True:
+        if not numerator%2 and not denominator%2:
+            numerator, denominator = numerator//2, denominator//2
+
+        elif not numerator%3 and not denominator%3:
+            numerator, denominator = numerator//3, denominator//3
+        
+        elif not numerator%5 and not denominator%5:
+            numerator, denominator = numerator//5, denominator//5
+        
+        elif not numerator%7 and not denominator%7:
+            numerator, denominator = numerator//7, denominator//7
+
+        else:
+            break
+    return numerator, denominator
+   
+
 # first_number = input("Введите первую дробь вида 1/2:\n").split("/")
 # second_number = input("Введите вторую дробь вида 1/2:\n").split("/")
-first_number = ("7","21")
+first_number = ("7","214")
 second_number = ("4","2")
+
 first_number = list(map(int, first_number))
 second_number = list(map(int, second_number))
 result = None
@@ -20,7 +40,7 @@ else:
     denominator = first_number[1] * second_number[1] 
     numerator = first_number[0] * second_number[1] + second_number[0] * first_number[1]
 
-
+numerator, denominator = Reduction(numerator, denominator)
 
 result = f"{numerator}/{denominator}"if numerator % denominator else int(numerator/denominator)
 print(f"\nМой результат(+) => \t{result}")
@@ -29,6 +49,8 @@ print(f"\nМой результат(+) => \t{result}")
 # Умножение дробей
 numerator = first_number[0] * second_number[0]
 denominator = first_number[1] * second_number[1]
+
+numerator, denominator = Reduction(numerator, denominator)
 
 result = f"{numerator}/{denominator}"if numerator % denominator else int(numerator/denominator)
 print(f"Мой результат(*) => \t{result}")
