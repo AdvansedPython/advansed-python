6ad4d94bdf091dd94af9762c0f5585ca899ec58e
neotyanka <neotyanka@yandex.ru>
2023-09-04 16:45:34 +0300

    edit 3 seminar 1 task

--- 3_seminar/DZ/1_Task.py
+++ 3_seminar/DZ/1_Task.py
@@ -7,30 +7,31 @@ items_all_friends = None
 item_one_friend = dict()
 items_all_friends = None
 
+# есть у всех
 for key, value in friends.items():
 
-    # есть у всех
     if items_all_friends == None:
         items_all_friends = value
     else:
         items_all_friends = set(items_all_friends) & set(value)
 
-    # есть только у одного
-    for key, value in friends.items():
-        staff = []
-        for key_friend, value_friend in friends.items():
-            if key != key_friend:
-                if staff == []:
-                    staff = set(value) - set(value_friend)
-                else:
-                    staff = set(staff) - set(value_friend)
 
-        if staff == set():
-            staff = "Печально, но нет уникальности("
-        item_one_friend[key] = staff
+# есть только у одного
+for key, value in friends.items():
+    staff = []
+    for key_friend, value_friend in friends.items():
+        if key != key_friend:
+            if staff == []:
+                staff = set(value) - set(value_friend)
+            else:
+                staff = set(staff) - set(value_friend)
 
-    # Есть у всех кроме одного
-    
+    if staff == set():
+        staff = "Печально, но нет уникальности("
+    item_one_friend[key] = staff
+
+# Есть у всех кроме одного
+
         
             
 
